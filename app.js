181860c840cc03058949b4c8f0be05464ff9a888
Sarthak431 <sssarthak431@gmail.com>
2024-10-17 12:57:31 +0530

    Basic Setup 2

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-import express from "express"
+import express from "express";
 
 const app = express();
 
-app.get("/",(req,res,next)=>{
-    res.send("Hi there");
+app.get("/", (req, res, next) => {
+  res.send("Hi there");
+});
+
+app.get("/health", (req, res, next) => {
+  res.json({ status: "OK", serverTime: new Date().toISOString() });
 });
 
 export default app;
